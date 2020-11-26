36b42e352c7e98ede073fe667bc5c5a57a6b2c39
Stefan Hezinger <stefan.hezinger@erbe-med.com>
2020-11-26 10:33:24 +0100

    refactored

--- vario.lua
+++ vario.lua
@@ -10,32 +10,38 @@ end
 
 local function run_func()
 
+    -- float 
     local stepSize = 1
-    local altitude = getValue('Tmp1')
-    
-    if altitude < 11 then
-        stepSize = 1
-    elseif altitude < 20 then
-        stepSize = 5
-    elseif altitude <= 100 then
-        stepSize = 10
-    else
-        stepSize = 20
-    end
-        
 
-  
-    local newAltitudeRange = math.floor(altitude / stepSize ) * stepSize
     
-    if newAltitudeRange >= currentAltitudeRange + stepSize  then
+    local altitude = getValue('Alt')
     
-        currentAltitudeRange = newAltitudeRange
-        playNumber(currentAltitudeRange, UNIT_METERS )
+    if altitude > -0.1 then 
+    
+        if altitude < 11 then
+            stepSize = 1
+        elseif altitude < 20 then
+            stepSize = 5
+        elseif altitude <= 100 then
+            stepSize = 10
+        else
+            stepSize = 20
+        end           
+            
+        local newAltitudeRange = 0
+        newAltitudeRange = math.floor(altitude / stepSize ) * stepSize
         
-    elseif newAltitudeRange <= currentAltitudeRange - stepSize then
+        if newAltitudeRange >= currentAltitudeRange + stepSize  then
+        
+            currentAltitudeRange = newAltitudeRange
+            playNumber(currentAltitudeRange, UNIT_METERS )
+            
+        elseif newAltitudeRange <= currentAltitudeRange - stepSize then
+        
+            playNumber(currentAltitudeRange, UNIT_METERS )
+            currentAltitudeRange = newAltitudeRange
+        end
     
-        playNumber(currentAltitudeRange, UNIT_METERS )
-        currentAltitudeRange = newAltitudeRange
     end
     
     
